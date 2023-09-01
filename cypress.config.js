2ee4ada79c6d42b9b2a1e59834bb616f85ef929d
tobyfagbo <oluwatobi@softcom.xyz>
2023-09-01 17:39:32 +0100

    Updated yml to run API test

--- cypress.config.js
+++ cypress.config.js
@@ -13,9 +13,7 @@ async function setupNodeEvents(on, config) {
 }
 
 module.exports = defineConfig({
-
-  reporter: 'cypress-mochawesome-reporter',
-
+  
   e2e: {
     baseUrl: 'https://ecommerce-playground.lambdatest.io/',
     defaultCommandTimeout: 15000,
@@ -24,9 +22,8 @@ module.exports = defineConfig({
     viewportWidth: 1400,
     specPattern: '**/*.feature',
     supportFile: 'cypress/support/e2e.js',
-    setupNodeEvents(on, config) {
-      require('cypress-mochawesome-reporter/plugin')(on);
-      },    
+    setupNodeEvents,
+    
   },
 
 });
